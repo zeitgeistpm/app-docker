12510273627f833503ee046763dd41a3cd1ad9e5
samuelarogbonlo <sbayo971@gmail.com>
2023-03-24 18:08:32 +0100

    Modify entrypoint.sh (#3)

--- ipfs/entrypoint.sh
+++ ipfs/entrypoint.sh
@@ -4,7 +4,7 @@
 ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["GET", "POST"]'
 ipfs config --json API.HTTPHeaders.Access-Control-Allow-Credentials '["true"]'
 ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin "[\"*\"]"
-ipfs config --json Addresses.API "/ip4/0.0.0.0/tcp/5001"
+ipfs config Addresses.API /ip4/0.0.0.0/tcp/5001
 
 # Run the CMD specified in the Dockerfile
 exec "$@"
