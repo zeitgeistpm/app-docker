8a60879c4570c52d9ee8454361cd745eee3b96e4
Samuel Arogbonlo <47984109+samuelarogbonlo@users.noreply.github.com>
2023-11-14 19:11:51 +0100

    Add IPFS Configuration (#7)
    * Initial Commit
    
    * Initial Commit
    
    * Edit docker-compose file version (#3)
    
    * Comment out some services (#3)
    
    * Edit container_name (#3)
    
    * Initialize ipfs daemon (#3)
    
    * Edit container_name and Service name (#3)
    
    * Edit volume config (#3)
    
    * Commit ports (#3)
    
    * Commit ports (#3)
    
    * Modify docker-compose.yaml (#3)
    
    * Add links (#3)
    
    * Add links (#3)
    
    * Add IPFS_IP env var (#3)
    
    * Change IPFS env format (#3)
    
    * Change IPFS env format (#3)
    
    * Change IPFS env format (#3)
    
    * Change IPFS network format (#3)
    
    * Modify volume (#3)
    
    * Modify entrypoint.sh (#3)
    
    * Modify entrypoint.sh (#3)
    
    * Remove ipfs-cluster
    
    * Remove networks and add common base path.
    
    Manually specfying the ip addresses is not necessary and is more likely to result in complications, as docker automatically associates the assigned ip with the hostname.
    
    * Use environment variables for ports
    
    * feat: Add autoheal for unhealthy container when healthcheck not passed
    
    ---------
    
    Co-authored-by: Harald Heckmann <mail@haraldheckmann.de>

---
+++ ipfs/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Set the IPFS configuration values
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["GET", "POST"]'
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Credentials '["true"]'
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin "[\"*\"]"
+ipfs config Addresses.API /ip4/0.0.0.0/tcp/5001
+
+# Run the CMD specified in the Dockerfile
+exec "$@"
